f22d2bb8dd4c3aae7b98a7defe8613124592bb36
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-06-01 17:56:42 -0400

    Add a button that will remove table columns by calling a JS function. Closes issue #4

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ const removeRows = () => {
     let table = document.getElementById("table");
     table.deleteRow(0);
 }
+
+/**
+ * Remove columns in the table (#4) by looping through the entire column
+ */
+const removeColumns = () => {
+    const table = document.getElementById("table");
+    let row = table.rows;
+
+    let index = 0;
+    for (let i = 0; i < row.length; i++) {
+        row[i].deleteCell(index);
+    }
+}
